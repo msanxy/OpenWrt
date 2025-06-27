a6e884a04e2749dd890c5996099d416cc9a199bd
msanxy <62334601+msanxy@users.noreply.github.com>
2025-06-28 03:43:14 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -50,7 +50,7 @@ git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-a
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 # git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
-git_sparse_clone master https://github.com/Thaolga/openwrt-nekobox luci-app-nekobox
+git clone --depth=1 https://github.com/Thaolga/openwrt-nekobox package/luci-app-nekobox
 
 # Themes
 git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
