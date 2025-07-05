2d42cb76374bf1fe89661ce0a0b1a5232be3ec9e
msanxy <62334601+msanxy@users.noreply.github.com>
2025-07-05 13:48:39 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -24,7 +24,7 @@ function git_sparse_clone() {
 
 # 晶晨宝盒
 git_sparse_clone main https://github.com/ophub/luci-app-amlogic luci-app-amlogic
-sed -i "s|firmware_repo.*|firmware_repo 'https://github.com/haiibo/OpenWrt'|g" package/luci-app-amlogic/root/etc/config/amlogic
+sed -i "s|firmware_repo.*|firmware_repo 'https://github.com/msanxy/OpenWrt'|g" package/luci-app-amlogic/root/etc/config/amlogic
 # sed -i "s|kernel_path.*|kernel_path 'https://github.com/ophub/kernel'|g" package/luci-app-amlogic/root/etc/config/amlogic
 sed -i "s|ARMv8|ARMv8_MINI|g" package/luci-app-amlogic/root/etc/config/amlogic
 
